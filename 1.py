61c6d772725c1d400b72e0c7e9a78e32c3a02771
Zhivlova <71148254+Zhivlova@users.noreply.github.com>
2022-08-08 13:59:24 +0300

    parsing

--- 1.py
+++ 1.py
@@ -1,107 +1,36 @@
-from urllib.request import urlopen
-from urllib.error import HTTPError
-from bs4 import BeautifulSoup
-import sys
-import ssl
 import requests
+from bs4 import BeautifulSoup
+import os
 import ssl
 
-ssl._create_default_https_context = ssl._create_unverified_context
+url_to_the_file = 'https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov/fileadmin/user_upload/ratings/state_institutions.xlsx'
 
+r = requests.get(url_to_the_file)
 
-def get_title(url):
-    try:
-        html = urlopen(url)
-    except HTTPError as e:
-        print(e)
-        return None
-    try:
-        soup = BeautifulSoup(html.read(), "lxml")
-        name_of_survey = soup.find(class_="h2-main")
-        for name in name_of_survey:
-            title = name_of_survey.text.strip()
-    except AttributeError as e:
-        return None
-    return title
+with open('xl.xlsx', 'wb') as f:
+    f.write(r.content)
 
+def get_xl_files(data_):
+    links = []
+    names_of_xl_files = []
+    for link in data_:
+        if ".xlsx" in link['href']:
+            print(link['href'])
+            links.append(link['href'])
+            names_of_xl_files.append(link.text)
 
-title = get_title('https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov')
-if title == None:
-    print(f"Название не найдено")
-else:
-    print(title)
+    if len(names_of_xl_files) == 0:
+        raise Exception
 
+    else:
+        for place in links:
+            with open(names_of_xl_files[place], 'wb', encoding="Windows-1251") as f:
+                content = requests.get(links[place]).content
+                f.write(content)
 
-def get_description(url):
-    try:
-        html = urlopen(url)
-    except HTTPError as e:
-        print(e)
-        return None
-    try:
-        soup = BeautifulSoup(html.read(), "lxml")
-        description_of_survey = soup.find(class_="frame frame-default frame-type-text frame-layout-0").find("p")
-        for desc in description_of_survey:
-            description = description_of_survey.text.strip()
-    except AttributeError as e:
-        return None
-    return description
+url = 'https://wciom.ru/fileadmin/user_upload/ratings/state_institutions.xlsx'
+result = requests.get(url).content
+soup = BeautifulSoup(result, 'html.parser')
+data = soup.find_all('a')
+get_xl_files(data)
 
-
-description = get_description('https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov')
-if description == None:
-    print(f"Описание не найдено")
-else:
-    print(description)
-
-
-def get_answer_options(url):
-    try:
-        html = urlopen(url)
-    except HTTPError as e:
-        print(e)
-        return None
-    try:
-        soup = BeautifulSoup(html.read(), "lxml")
-        answer_options = soup.find(class_="frame frame-default frame-type-text frame-layout-0").find_all("li")
-        president = answer_options[0].text
-        prime_minister = answer_options[1].text
-        governments = answer_options[2].text
-        duma = answer_options[3].text
-        federation = answer_options[4].text
-    except AttributeError as e:
-        return None
-    return president, prime_minister, governments, duma, federation
-
-
-answer_options = get_answer_options('https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov')
-if answer_options == None:
-    print(f"Ответы не найдены")
-else:
-    print(answer_options)
-
-
-def get_all_download_links(url):
-    try:
-        html = urlopen(url)
-    except HTTPError as e:
-        print(e)
-        return None
-    try:
-        soup = BeautifulSoup(html.read(), "lxml")
-        all_download_links = soup.find_all(class_="ce-uploads")
-        for link in all_download_links:
-            table_link = 'https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov' + link.find('a').get(
-                'href')
-            print(table_link)
-
-    except AttributeError as e:
-        return None
-    return table_link
-
-
-all_download_links = get_all_download_links('https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov')
-if all_download_links == None:
-    print(f"Ссылки на скачивание не найдены")
-else:
-    print(all_download_links)
--- parsing.py
+++ parsing.py
@@ -1,105 +1,122 @@
 import csv
-import json
-from collections import ChainMap
-import requests
+from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
-import urllib.request
+import requests
+import ssl
 
 
-def get_data(url):
-    """Получаем код странцицы"""
-    headers = {
-        "Accept": "*/*",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/103.0.0.0 Safari/537.36 "
-    }
+ssl._create_default_https_context = ssl._create_unverified_context
 
-    req = requests.get(url, headers=headers)
-    src = req.text
+"""название опроса"""
 
-    """Сохраняем код страницы"""
-    with open("dejatelnost-gosudarstvennykh-institutov.html", "w") as file:
-        file.write(src)
-
-    with open("dejatelnost-gosudarstvennykh-institutov.html") as file:
-        src = file.read()
-
-    soup = BeautifulSoup(src, "lxml")
-    all_data = {}
-
+def get_title(url):
     try:
-        """название опроса"""
+        html = urlopen(url)
+    except HTTPError as e:
+        print(e)
+        return None
+    try:
+        soup = BeautifulSoup(html.read(), "lxml")
         name_of_survey = soup.find(class_="h2-main")
         for name in name_of_survey:
-            all_data['name_of_survey'] = name_of_survey.text.strip()
+            title = name_of_survey.text.strip()
+    except AttributeError as e:
+        return None
+    return title
 
-        """описание опроса"""
+title = get_title('https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov')
+if title == None:
+    print(f"Название не найдено")
+else:
+    print(title)
+
+"""описание опроса"""
+
+def get_description(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        print(e)
+        return None
+    try:
+        soup = BeautifulSoup(html.read(), "lxml")
         description_of_survey = soup.find(class_="frame frame-default frame-type-text frame-layout-0").find("p")
         for desc in description_of_survey:
-            title = description_of_survey.text.strip()
-            all_data['description_of_survey'] = description_of_survey.text.strip()
+            description = description_of_survey.text.strip()
+    except AttributeError as e:
+        return None
+    return description
 
-        """варианты ответа"""
+description = get_description('https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov')
+if description == None:
+    print(f"Описание не найдено")
+else:
+    print(description)
+
+"""варианты ответа"""
+def get_answer_options(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        print(e)
+        return None
+    try:
+        soup = BeautifulSoup(html.read(), "lxml")
         answer_options = soup.find(class_="frame frame-default frame-type-text frame-layout-0").find_all("li")
         president = answer_options[0].text
         prime_minister = answer_options[1].text
         governments = answer_options[2].text
         duma = answer_options[3].text
         federation = answer_options[4].text
+    except AttributeError as e:
+        return None
+    return president, prime_minister, governments, duma, federation
 
-        for answer in answer_options:
-            all_data['answer_options'] = answer.text
-            print(all_data)
 
-        """ссылки на скачивание"""
+answer_options = get_answer_options('https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov')
+if answer_options == None:
+    print(f"Ответы не найдены")
+else:
+    print(answer_options)
+
+"""ссылки на скачивание"""
+def get_all_download_links(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        print(e)
+        return None
+    try:
+        soup = BeautifulSoup(html.read(), "lxml")
         all_download_links = soup.find_all(class_="ce-uploads")
         for link in all_download_links:
             table_link = 'https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov' + link.find('a').get(
                 'href')
-            # table_link_1 = table_link[0]
-            # table_link_2 = table_link[1]
+            print(table_link)
 
-            all_data['all_download_links'] = table_link
-            all_data['all_download_links'] = table_link
+    except AttributeError as e:
+        return None
+    return table_link
 
-    except TypeError as e:
-        print(f" [TypeError]: {e.strerror}, filename: {e.filename}")
-    else:
-        print("Writing to file...")
-
-        with open('all_data.csv', "w", encoding="utf-8") as file:
-            writer = csv.writer(file)
-            writer.writerow(name_of_survey)
-
-        with open('all_data.csv', "a", encoding="utf-8") as file:
-            writer = csv.writer(file)
-            writer.writerow(title)
-
-        with open('all_data.csv', "a", encoding="utf-8") as file:
-            writer = csv.writer(file)
-            writer.writerow(
-                (president,
-                 prime_minister,
-                 governments,
-                 duma,
-                 federation
-                 )
-            )
-
-        with open('all_data.csv', "a", encoding="utf-8") as file:
-            writer = csv.writer(file)
-            writer.writerow(
-                (
-                    table_link,
-                    table_link
-                )
-            )
+all_download_links = get_all_download_links('https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov')
+if all_download_links == None:
+    print(f"Ссылки на скачивание не найдены")
+else:
+    print(all_download_links)
 
 
-print("Program started")
-print(get_data("https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov/"))
-print("Program finished")
+"""записываем данные"""
 
+myData = [["title", "description", "answer", "links"],
+          [title, description, answer_options, all_download_links]]
 
-url = 'https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov/fileadmin/user_upload/ratings/state_institutions.xlsx'
-urllib.request.urlretrieve(url, 'cat.jpg')
+myFile = open('all_data.csv', 'w')
+with myFile:
+    writer = csv.writer(myFile)
+    writer.writerows(myData)
+
+print("Writing complete")
+
+r = requests.get('https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov')
+print(r)
