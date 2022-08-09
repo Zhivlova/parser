f0cc4bfd7f3822aa187d057e4214d587412bdf20
Zhivlova <71148254+Zhivlova@users.noreply.github.com>
2022-08-09 23:34:43 +0300

    парсинг

---
+++ 0908.py
@@ -0,0 +1,72 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def find_download_buttons(url):
+    buttons = set()
+    html = urlopen(url)
+    soup = BeautifulSoup(html.read(), "lxml")
+    all_download_links = soup.find_all(class_="ce-uploads")
+    for link in all_download_links:
+        table_link = 'https://wciom.ru' + link.find('a').get(
+            'href')
+        buttons.add(table_link)
+        # возвращаем пронумерованный set()
+        for index, value in enumerate(buttons, 1):
+            print(index, value)
+    return buttons
+
+print(find_download_buttons('https://wciom.ru/ratings/indeks-potrebitelskogo-doverija'))
+
+
+def download_from_buttons(url):
+    filename = url.split('/')[-1]
+    filename = 'wciom' + '_' + filename
+    r = requests.get(url, allow_redirects=True)
+    open(filename, "wb").write(r.content)
+    print(f"{filename}  - Writing complete")
+
+download_from_buttons(
+    'https://wciom.ru/fileadmin/user_upload/ratings/pokupki_doverie.xls')
+
+
+def get_title(url):
+    html = urlopen(url)
+    soup = BeautifulSoup(html.read(), "lxml")
+    name_of_survey = soup.find(class_="h2-main")
+    for name in name_of_survey:
+        title = name_of_survey.text.strip()
+        print(title)
+        return title
+
+
+get_title('https://wciom.ru/ratings/indeks-potrebitelskogo-doverija')
+
+
+def get_description(url):
+    html = urlopen(url)
+    soup = BeautifulSoup(html.read(), "lxml")
+    description_of_survey = soup.find(class_="frame frame-default frame-type-text frame-layout-0").find("p")
+    for desc in description_of_survey:
+        description = desc.text.strip()
+        print(description)
+    return description
+
+
+get_description('https://wciom.ru/ratings/indeks-potrebitelskogo-doverija')
+
+
+def get_answer_options(url):
+    html = urlopen(url)
+    soup = BeautifulSoup(html.read(), "lxml")
+    answer_options = soup.find(class_="frame frame-default frame-type-text frame-layout-0").find_all("li")
+    for answer in answer_options:
+        answers = answer.text.strip()
+        print(answers)
+
+
+get_answer_options('https://wciom.ru/ratings/indeks-potrebitelskogo-doverija')
--- 1.py
+++ 1.py
@@ -3,34 +3,41 @@ from bs4 import BeautifulSoup
 import os
 import ssl
 
-url_to_the_file = 'https://wciom.ru/ratings/dejatelnost-gosudarstvennykh-institutov/fileadmin/user_upload/ratings/state_institutions.xlsx'
+link = 'https://wciom.ru/ratings/indeks-potrebitelskogo-doverija'
 
-r = requests.get(url_to_the_file)
+def save_from_www(link):
+    filename = link.split('/')[-1]
+    print(filename)
+    r = requests.get(link, allow_redirects=True)
+    open(filename, "wb").write(r.content)
 
-with open('xl.xlsx', 'wb') as f:
-    f.write(r.content)
+save_from_www(link)
 
-def get_xl_files(data_):
-    links = []
-    names_of_xl_files = []
-    for link in data_:
-        if ".xlsx" in link['href']:
-            print(link['href'])
-            links.append(link['href'])
-            names_of_xl_files.append(link.text)
-
-    if len(names_of_xl_files) == 0:
-        raise Exception
-
-    else:
-        for place in links:
-            with open(names_of_xl_files[place], 'wb', encoding="Windows-1251") as f:
-                content = requests.get(links[place]).content
-                f.write(content)
-
-url = 'https://wciom.ru/fileadmin/user_upload/ratings/state_institutions.xlsx'
-result = requests.get(url).content
-soup = BeautifulSoup(result, 'html.parser')
-data = soup.find_all('a')
-get_xl_files(data)
-
+# r = requests.get(url_to_the_file)
+#
+# with open('xl.xlsx', 'wb') as f:
+#     f.write(r.content)
+#
+# def get_xl_files(data_):
+#     links = []
+#     names_of_xl_files = []
+#     for link in data_:
+#         if ".xlsx" in link['href']:
+#             print(link['href'])
+#             links.append(link['href'])
+#             names_of_xl_files.append(link.text)
+#
+#     if len(names_of_xl_files) == 0:
+#         raise Exception
+#
+#     else:
+#         for place in links:
+#             with open(names_of_xl_files[place], 'wb', encoding="Windows-1251") as f:
+#                 content = requests.get(links[place]).content
+#                 f.write(content)
+#
+# url = 'https://wciom.ru/fileadmin/user_upload/ratings/state_institutions.xlsx'
+# result = requests.get(url).content
+# soup = BeautifulSoup(result, 'html.parser')
+# data = soup.find_all('a')
+# get_xl_files(data)
