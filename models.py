3b305368e0075f343dfb85de384c7dfc1265d096
Zhivlova <71148254+Zhivlova@users.noreply.github.com>
2022-08-19 00:42:24 +0300

    excel

--- models.py
+++ models.py
@@ -176,31 +176,181 @@ class Behavioral_parameters:
         """Импортный тариф"""
         # Базовое рановесие
         def base_import_tariff(self, P_MMI_0, Q_MMI_0, TMMI_0):
-            base = (P_MMI_0*Q_MMI_0)*TMMI_0
-            return base
+            base_import_tariff = (P_MMI_0*Q_MMI_0)*TMMI_0
+            return base_import_tariff
         # Новое равновесие
         def new_import_tariff(self, P_MMI_1, Q_MMI_1, TMMI_1):
-            new = (P_MMI_1*Q_MMI_1)*TMMI_1
-            return new
+            new_import_tariff = (P_MMI_1*Q_MMI_1)*TMMI_1
+            return new_import_tariff
         # Изменение млн. руб
-        def change_import_tariff(self, new, base):
-            return  new - base
+        def change_import_tariff(self, new_import_tariff, base_import_tariff):
+            change_import_tariff = new_import_tariff - base_import_tariff
+            return  change_import_tariff
         # Изменение % к начальному
-        def change_pr_import_tariff(self, new, base):
-            return  new / base - 1
+        def change_pr_import_tariff(self, new_import_tariff, base_import_tariff):
+            return  new_import_tariff / base_import_tariff - 1
 
         """Производство макулатурных тарных картонов"""
         # Базовое рановесие
-
+        def base_production_wastepaper(self, P_SDP_0, Q_SDP_0, Tax, Lab, Q_SDP_0):
+            base_production_wastepaper = P_SDP_0*Q_SDP_0*Tax+Lab*Q_SDP_0*47/130
+            return base_production_wastepaper
         # Новое равновесие
+        def new_production_wastepaper(self, P_SDP_1, Q_SDP_1, Tax, Lab, Q_SDP_1):
+            new_production_wastepaper = P_SDP_1*Q_SDP_1*Tax+Lab*Q_SDP_1*47/130
+            return new_production_wastepaper
         # Изменение млн. руб
+        def change_production_wastepaper(self, base_production_wastepaper, new_production_wastepaper):
+            return new_production_wastepaper - base_production_wastepaper
         # Изменение % к начальному
+        def change_pr_production_wastepaper(self,  new_production_wastepaper, base_production_wastepaper):
+            return new_production_wastepaper / base_production_wastepaper - 1
 
+        """Производство целлюлозных тарных картонов"""
+        # Базовое рановесие
+        def base_production_cellulose(self, Tax, P_FDP_0, Q_FDP_0, Lab, Q_FDP_0):
+            base_production_cellulose = Tax*P_FDP_0*Q_FDP_0+Lab*Q_FDP_0*47/130
+            return base_production_cellulose
+        # Новое равновесие
+        def new_production_cellulose(self, Tax, P_FDP_1, Q_FDP_1, Lab, Q_FDP_1):
+            new_production_cellulose = Tax*P_FDP_1*Q_FDP_1+Lab*Q_FDP_1*47/130
+            return new_production_cellulose
+        # Изменение млн. руб
+        def change_production_cellulose(self, new_production_cellulose, base_production_cellulose):
+            return new_production_cellulose - base_production_cellulose
+        # Изменение % к начальному
+        def change_pr_production_cellulose(self,  new_production_cellulose, base_production_cellulose):
+            return new_production_cellulose / base_production_cellulose - 1
 
+        """Налог на потребителей тарных картонов (налог на прибыль)"""
+        # Базовое рановесие
+        def base_tax_consumers(self,P_PDP_0, Q_PDP_0, 'Рентабельность (ФНС) !B15', VAT):
+            base_tax_consumers = (P_PDP_0*Q_PDP_0)*'Рентабельность (ФНС) !B15'/100*VAT
+            return base_tax_consumers
+        # Новое равновесие
+        def new_tax_consumers(self, base_tax_consumers, change_tax_consumers):
+            new_tax_consumers = base_tax_consumers + change_tax_consumers
+            return new_tax_consumers
+        # Изменение млн. руб
+        def change_tax_consumers(self, P_PDP_0, P_PDP_1,Q_PDP_1, Q_PDP_0):
+            change_tax_consumers =  ((P_PDP_0-P_PDP_1)*(Q_PDP_1+Q_PDP_0)/2)*0,2
+            return change_tax_consumers
+        # Изменение % к начальному
+        def change_pr_tax_consumers(self, change_tax_consumers, base_tax_consumers):
+            return change_tax_consumers / base_tax_consumers
 
+        """Итого налоги"""
 
         # Базовое рановесие
+        def base_total_taxes(self, base_import_tariff, base_production_wastepaper, base_production_cellulose, base_tax_consumers):
+            base_total_taxes = base_import_tariff + base_production_wastepaper + base_production_cellulose + base_tax_consumers
+            return base_total_taxes
         # Новое равновесие
+        def new_total_taxes(self, base_total_taxes, change_tax_consumers):
+            return base_total_taxes + change_tax_consumers
         # Изменение млн. руб
+        def change_total_taxes(self, change_import_tariff,change_production_wastepaper, change_production_cellulose, change_tax_consumers):
+            change_total_taxes = change_import_tariff + change_production_wastepaper + change_production_cellulose + change_tax_consumers
+            return change_total_taxes
         # Изменение % к начальному
+        def change_pr_total_taxes(self, change_total_taxes, base_total_taxes):
+            return change_total_taxes / base_total_taxes
 
+        """Изменение прибыли потребителей картона"""
+
+        # Базовое рановесие
+        def base_change_profit_consumers(self, P_PDP_0, Q_PDP_0, 'Рентабельность (ФНС) !B15'):
+            base_change_profit_consumers = (P_PDP_0 * Q_PDP_0) * 'Рентабельность (ФНС) !B15' / 100 * 0, 8
+            return base_change_profit_consumers
+        # Новое равновесие
+        def new_change_profit_consumers(self, base_change_profit_consumers, ch_change_profit_consumers):
+            new_change_profit_consumers = base_change_profit_consumers + ch_change_profit_consumers
+            return new_change_profit_consumers
+        # Изменение млн. руб
+        def ch_change_profit_consumers(self, P_PDP_0, P_PDP_1, Q_PDP_1, Q_PDP_0):
+            ch_change_profit_consumers = ((P_PDP_0-P_PDP_1)*(Q_PDP_1+Q_PDP_0)/2)*0,8
+            return ch_change_profit_consumers
+        # Изменение % к начальному
+        def ch_pr_change_profit_consumers(self, ch_change_profit_consumers, base_change_profit_consumers):
+            ch_pr_change_profit_consumers = ch_change_profit_consumers / base_change_profit_consumers
+            return ch_pr_change_profit_consumers
+
+        """Изменение прибыли производителей макулатурных картонов"""
+
+        # Базовое рановесие
+        def base_change_profit_manufacturers(self, P_SDP_0, Q_SDP_0, profit):
+            base_change_profit_manufacturers = P_SDP_0*Q_SDP_0*profit*0,8
+            return base_change_profit_manufacturers
+        # Новое равновесие
+        def new_change_profit_manufacturers(self, base_change_profit_manufacturers, ch_change_profit_manufacturers):
+            new_change_profit_manufacturers = base_change_profit_manufacturers + ch_change_profit_manufacturers
+            return new_change_profit_manufacturers
+        # Изменение млн. руб
+        def ch_change_profit_manufacturers(self,P_SDP_1, Q_SDP_1, P_SDP_0, Q_SDP_0, profit)
+            ch_change_profit_manufacturers = (P_SDP_1*Q_SDP_1-P_SDP_0*Q_SDP_0)*profit*0,8
+            return ch_change_profit_manufacturers
+        # Изменение % к начальному
+        def ch_pr_change_profit_manufacturers(self, ch_change_profit_manufacturers, base_change_profit_manufacturers):
+            ch_pr_change_profit_manufacturers = ch_change_profit_manufacturers / base_change_profit_manufacturers
+            return ch_pr_change_profit_manufacturers
+
+        """Изменение прибыли производителей целлюлозных картонов"""
+
+        # Базовое рановесие
+        def base_change_profit_cellulose_manufacturers(self,P_FDP_0, Q_FDP_0, profit):
+            base_change_profit_cellulose_manufacturers = P_FDP_0*Q_FDP_0*profit*0,8
+            return base_change_profit_cellulose_manufacturers
+        # Новое равновесие
+        def new_change_profit_cellulose_manufacturers(self, base_change_profit_cellulose_manufacturers, ch_change_profit_cellulose_manufacturers):
+            new_change_profit_cellulose_manufacturers = base_change_profit_cellulose_manufacturers + ch_change_profit_cellulose_manufacturers
+            return new_change_profit_cellulose_manufacturers
+        # Изменение млн. руб
+        def ch_change_profit_cellulose_manufacturers(self, P_FDP_1, Q_FDP_1, P_FDP_0, Q_FDP_0, profit):
+            ch_change_profit_cellulose_manufacturers = (P_FDP_1*Q_FDP_1-P_FDP_0*Q_FDP_0)*profit*0,8
+            return ch_change_profit_cellulose_manufacturers
+        # Изменение % к начальному
+        def ch_pr_change_profit_cellulose_manufacturers(self, ch_change_profit_cellulose_manufacturers, base_change_profit_cellulose_manufacturers):
+            ch_pr_change_profit_cellulose_manufacturers = ch_change_profit_cellulose_manufacturers / base_change_profit_cellulose_manufacturers
+            return ch_pr_change_profit_cellulose_manufacturers
+
+        """Изменение совокупной зарплаты сотрудников отрасли тарных картонов"""
+
+        # Базовое рановесие
+        def base_change_salary_employees(self, Q_SDP_0, Lab, Q_FDP_0):
+            base_change_salary_employees = Q_SDP_0*Lab+Q_FDP_0*Lab
+            return base_change_salary_employees
+        # Новое равновесие
+        def new_change_salary_employees(self, base_change_salary_employees, ch_change_salary_employees):
+            new_change_salary_employees = base_change_salary_employees + ch_change_salary_employees
+            return new_change_salary_employees
+        # Изменение млн. руб
+        def ch_change_salary_employees(self, Q_SDP_1, Q_SDP_0, Lab, Q_FDP_1, Q_FDP_0):
+            ch_change_salary_employees = (Q_SDP_1-Q_SDP_0)*Lab+(Q_FDP_1-Q_FDP_0)*Lab
+            return ch_change_salary_employees
+        # Изменение % к начальному
+        def ch_pr_change_salary_employees(self, ch_change_salary_employees, base_change_salary_employees):
+            ch_pr_change_salary_employees = ch_change_salary_employees / base_change_salary_employees
+            return ch_pr_change_salary_employees
+
+        """Итого благосостояние"""
+
+        # Базовое рановесие
+        def base_total_wealth(self, base_change_profit_consumers, base_change_profit_manufacturers,
+                              base_change_profit_cellulose_manufacturers, base_change_salary_employees):
+            base_total_wealth = base_change_profit_consumers + base_change_profit_manufacturers + \
+                                base_change_profit_cellulose_manufacturers + base_change_salary_employees
+            return base_total_wealth
+        # Новое равновесие
+        def new_total_wealth(self, base_total_wealth, change_total_wealth):
+            new_total_wealth = base_total_wealth + change_total_wealth
+            return new_total_wealth
+        # Изменение млн. руб
+        def change_total_wealth(self, ch_change_profit_consumers, ch_change_profit_manufacturers,
+                                ch_change_profit_cellulose_manufacturers, ch_change_salary_employees):
+            change_total_wealth = ch_change_profit_consumers + ch_change_profit_manufacturers + \
+                                  ch_change_profit_cellulose_manufacturers + ch_change_salary_employees
+            return change_total_wealth
+        # Изменение % к начальному
+        def change_pr_total_wealth(self, change_total_wealth, base_total_wealth):
+            change_pr_total_wealth = change_total_wealth / base_total_wealth
+            return change_pr_total_wealth
