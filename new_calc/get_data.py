99e8b85c6c82299a6a2788f28b71c9083c8cda7b
Zhivlova <71148254+Zhivlova@users.noreply.github.com>
2022-10-06 16:36:24 +0300

    pandas parser

--- new_calc/get_data.py
+++ new_calc/get_data.py
@@ -14,10 +14,10 @@ example_data = {'PW_B1_before': 1500.0, 'PW_B1_after': 1500.0, 'PW_B2_before': 3
 
 user_data = {'PW_B1_before': 1500.0, 'PW_B1_after': 1500.0, 'PW_B2_before': 300.0, 'PW_B2_after': 300.0,
                 'ER_before': 75.0, 'ER_after': 75.0, 'TD_before': 0.0, 'TD_after': 0.0, 'Pb_B1_before': 82500.0,
-                'Pb_B1_after': 89500.0, 'tb_B1_before': 0.0, 'tb_B1_after': 0.7, 'Pb_B2_before': 13875.0,
+                'Pb_B1_after': 82500.0, 'tb_B1_before': 0.0, 'tb_B1_after': 0.7, 'Pb_B2_before': 13875.0,
                 'Pb_B2_after': 13875.0, 'tb_B2_before': 0.7, 'tb_B2_after': 0.7, 'PI_B1': 90000.0, 'PI_B2': 15000.0,
-                'PI_A': 40000.0, 'QSI_A': 15.0, 'QSW_RUS_A_before': 0.0, 'QSW_RUS_A_after': 0.0, 'i_cost_before': 1.0,
-                'i_cost_after': 1.0, 'shift_QSI_A_before': 0.0, 'shift_QSI_A_after': 0.0, 'PI_С': 130000.0,
+                'PI_A': 40000.0, 'QSI_A': 15.0, 'QSW_RUS_A_before': 0.0, 'QSW_RUS_A_after': 0.0, 'i_cost_before': 2.0,
+                'i_cost_after': 1.0, 'shift_QSI_A_before': 2.0, 'shift_QSI_A_after': 0.0, 'PI_С': 130000.0,
                 'QDI_С': 3.0, 'QDI_B2': 2.0}
 
 
@@ -78,9 +78,9 @@ def oil_export(input_data):
     # print(output_coefficients.to_markdown())
 
     # Цены на мировом рынке товаров группы B
-    prices_on_world_market_of_group_b_products = df.iloc[1:7, 3:9]
-    prices_on_world_market_of_group_b_products.index = np.arange(0, len(prices_on_world_market_of_group_b_products))
-    prices_on_world_market_of_group_b_products = prices_on_world_market_of_group_b_products.rename(columns={
+    prices_group_b_products = df.iloc[1:7, 3:9]
+    prices_group_b_products.index = np.arange(0, len(prices_group_b_products))
+    prices_group_b_products = prices_group_b_products.rename(columns={
         'Unnamed: 4': 'title',
         'Unnamed: 5': 'currency',
         'Unnamed: 6': 'designation',
@@ -90,9 +90,9 @@ def oil_export(input_data):
     })
 
     # Расчет суммы вывозной таможенной пошлины
-    calculation_of_amount_of_export_customs_duty = df.iloc[9:17, 3:9]
-    calculation_of_amount_of_export_customs_duty.index = np.arange(0, len(calculation_of_amount_of_export_customs_duty))
-    calculation_of_amount_of_export_customs_duty = calculation_of_amount_of_export_customs_duty.rename(columns={
+    calc_export_customs_duty = df.iloc[9:17, 3:9]
+    calc_export_customs_duty.index = np.arange(0, len(calc_export_customs_duty))
+    calc_export_customs_duty = calc_export_customs_duty.rename(columns={
         'Unnamed: 4': 'title',
         'Unnamed: 5': 'measure',
         'Unnamed: 6': 'designation',
@@ -126,9 +126,9 @@ def oil_export(input_data):
     })
 
     # Внутреннее производство и баланс товара А
-    internal_production_and_balance_of_goods_a = df.iloc[28:33, 3:9]
-    internal_production_and_balance_of_goods_a.index = np.arange(0, len(internal_production_and_balance_of_goods_a))
-    internal_production_and_balance_of_goods_a = internal_production_and_balance_of_goods_a.rename(columns={
+    int_prod_balance_of_goods_a = df.iloc[28:33, 3:9]
+    int_prod_balance_of_goods_a.index = np.arange(0, len(int_prod_balance_of_goods_a))
+    int_prod_balance_of_goods_a = int_prod_balance_of_goods_a.rename(columns={
         'Unnamed: 4': 'title',
         'Unnamed: 5': 'measure',
         'Unnamed: 6': 'designation',
@@ -203,26 +203,57 @@ def oil_export(input_data):
     # вводим новые значения
 
     # Цены на мировом рынке товаров группы B
-    prices_on_world_market_of_group_b_products.at[0, 'before'] = input_data.PW_B1_before
-    prices_on_world_market_of_group_b_products.at[0, 'after'] = input_data.PW_B1_after
-    prices_on_world_market_of_group_b_products.at[1, 'before'] = input_data.PW_B2_before
-    prices_on_world_market_of_group_b_products.at[1, 'after'] = input_data.PW_B2_after
-    prices_on_world_market_of_group_b_products.at[4, 'before'] = input_data.ER_before
-    prices_on_world_market_of_group_b_products.at[4, 'after'] = input_data.ER_after
-    prices_on_world_market_of_group_b_products.at[5, 'before'] = input_data.TD_before
-    prices_on_world_market_of_group_b_products.at[5, 'after'] = input_data.TD_after
-    # print(prices_on_world_market_of_group_b_products.to_markdown())
+    prices_group_b_products.at[0, 'before'] = input_data.PW_B1_before
+    prices_group_b_products.at[0, 'after'] = input_data.PW_B1_after
+    prices_group_b_products.at[0, 'status'] = prices_group_b_products['status'].pipe(
+        lambda x: 'Параметр изменен' if prices_group_b_products.at[0, 'before']
+                                        != prices_group_b_products.at[0, 'after'] else 'Параметр не изменен')
+
+    prices_group_b_products.at[1, 'before'] = input_data.PW_B2_before
+    prices_group_b_products.at[1, 'after'] = input_data.PW_B2_after
+    prices_group_b_products.at[1, 'status'] = prices_group_b_products['status'].pipe(
+        lambda x: 'Параметр изменен' if prices_group_b_products.at[1, 'before']
+                                        != prices_group_b_products.at[1, 'after'] else 'Параметр не изменен')
+
+    prices_group_b_products.at[4, 'before'] = input_data.ER_before
+    prices_group_b_products.at[4, 'after'] = input_data.ER_after
+    prices_group_b_products.at[4, 'status'] = prices_group_b_products['status'].pipe(
+        lambda x: 'Параметр изменен' if prices_group_b_products.at[4, 'before']
+                                        != prices_group_b_products.at[4, 'after'] else 'Параметр не изменен')
+
+    prices_group_b_products.at[5, 'before'] = input_data.TD_before
+    prices_group_b_products.at[5, 'after'] = input_data.TD_after
+    prices_group_b_products.at[5, 'status'] = prices_group_b_products['status'].pipe(
+        lambda x: 'Параметр изменен' if prices_group_b_products.at[5, 'before']
+                                        != prices_group_b_products.at[5, 'after'] else 'Параметр не изменен')
+
+    # print(prices_group_b_products.to_markdown())
 
     # Расчет суммы вывозной таможенной пошлины
-    calculation_of_amount_of_export_customs_duty.at[0, 'before'] = input_data.Pb_B1_before
-    calculation_of_amount_of_export_customs_duty.at[0, 'after'] = input_data.Pb_B1_after
-    calculation_of_amount_of_export_customs_duty.at[1, 'before'] = input_data.tb_B1_before
-    calculation_of_amount_of_export_customs_duty.at[1, 'after'] = input_data.tb_B1_after
-    calculation_of_amount_of_export_customs_duty.at[4, 'before'] = input_data.Pb_B2_before
-    calculation_of_amount_of_export_customs_duty.at[4, 'after'] = input_data.Pb_B2_after
-    calculation_of_amount_of_export_customs_duty.at[5, 'before'] = input_data.tb_B2_before
-    calculation_of_amount_of_export_customs_duty.at[5, 'after'] = input_data.tb_B2_after
-    # print(calculation_of_amount_of_export_customs_duty.to_markdown())
+    calc_export_customs_duty.at[0, 'before'] = input_data.Pb_B1_before
+    calc_export_customs_duty.at[0, 'after'] = input_data.Pb_B1_after
+    calc_export_customs_duty.at[0, 'status'] = calc_export_customs_duty['status'].pipe(
+        lambda x: 'Параметр изменен' if calc_export_customs_duty.at[0, 'before']
+                                        != calc_export_customs_duty.at[0, 'after'] else 'Параметр не изменен')
+
+    calc_export_customs_duty.at[1, 'before'] = input_data.tb_B1_before
+    calc_export_customs_duty.at[1, 'after'] = input_data.tb_B1_after
+    calc_export_customs_duty.at[1, 'status'] = calc_export_customs_duty['status'].pipe(
+        lambda x: 'Параметр изменен' if calc_export_customs_duty.at[1, 'before']
+                                        != calc_export_customs_duty.at[1, 'after'] else 'Параметр не изменен')
+
+    calc_export_customs_duty.at[4, 'before'] = input_data.Pb_B2_before
+    calc_export_customs_duty.at[4, 'after'] = input_data.Pb_B2_after
+    calc_export_customs_duty.at[4, 'status'] = calc_export_customs_duty['status'].pipe(
+        lambda x: 'Параметр изменен' if calc_export_customs_duty.at[4, 'before']
+                                        != calc_export_customs_duty.at[4, 'after'] else 'Параметр не изменен')
+
+    calc_export_customs_duty.at[5, 'before'] = input_data.tb_B2_before
+    calc_export_customs_duty.at[5, 'after'] = input_data.tb_B2_after
+    calc_export_customs_duty.at[5, 'status'] = calc_export_customs_duty['status'].pipe(
+        lambda x: 'Параметр изменен' if calc_export_customs_duty.at[5, 'before']
+                                        != calc_export_customs_duty.at[5, 'after'] else 'Параметр не изменен')
+    # print(calc_export_customs_duty.to_markdown())
 
     # Внутренний рынок товаров группы B
     domestic_market_of_group_b_products.at[0, 'before'] = input_data.PI_B1
@@ -234,14 +265,20 @@ def oil_export(input_data):
     # print(internal_market_of_product_a.to_markdown())
 
     # Внутреннее производство и баланс товара А
-    internal_production_and_balance_of_goods_a.at[0, 'before'] = input_data.QSI_A
-    internal_production_and_balance_of_goods_a.at[1, 'before'] = input_data.QSW_RUS_A_before
-    internal_production_and_balance_of_goods_a.at[1, 'after'] = input_data.QSW_RUS_A_after
-    internal_production_and_balance_of_goods_a.at[2, 'before'] = input_data.i_cost_before
-    internal_production_and_balance_of_goods_a.at[2, 'after'] = input_data.i_cost_after
-    internal_production_and_balance_of_goods_a.at[3, 'before'] = input_data.shift_QSI_A_before
-    internal_production_and_balance_of_goods_a.at[3, 'after'] = input_data.shift_QSI_A_after
-    # print(internal_production_and_balance_of_goods_a.to_markdown())
+    int_prod_balance_of_goods_a.at[0, 'before'] = input_data.QSI_A
+    int_prod_balance_of_goods_a.at[1, 'before'] = input_data.QSW_RUS_A_before
+    int_prod_balance_of_goods_a.at[1, 'after'] = input_data.QSW_RUS_A_after
+    int_prod_balance_of_goods_a.at[2, 'before'] = input_data.i_cost_before
+    int_prod_balance_of_goods_a.at[2, 'after'] = input_data.i_cost_after
+    int_prod_balance_of_goods_a.at[2, 'status'] = int_prod_balance_of_goods_a['status'].pipe(
+        lambda x: 'Параметр изменен' if int_prod_balance_of_goods_a.at[2, 'before']
+                                        != int_prod_balance_of_goods_a.at[2, 'after'] else 'Параметр не изменен')
+    int_prod_balance_of_goods_a.at[3, 'before'] = input_data.shift_QSI_A_before
+    int_prod_balance_of_goods_a.at[3, 'after'] = input_data.shift_QSI_A_after
+    int_prod_balance_of_goods_a.at[3, 'status'] = int_prod_balance_of_goods_a['status'].pipe(
+        lambda x: 'Параметр изменен' if int_prod_balance_of_goods_a.at[3, 'before']
+                                        != int_prod_balance_of_goods_a.at[3, 'after'] else 'Параметр не изменен')
+    print(int_prod_balance_of_goods_a.to_markdown())
 
     # Внутреннее производство товара С
     internal_production_of_goods_c.at[0, 'before'] = input_data.PI_С
@@ -252,21 +289,21 @@ def oil_export(input_data):
     production_and_balance_of_group_b_goods.at[4, 'before'] = input_data.QDI_B2
     # print(production_and_balance_of_group_b_goods.to_markdown())
 
-
     result_to_front = {}
 
     result_to_front[
-        'prices_on_world_market_of_group_b_products'] = prices_on_world_market_of_group_b_products.to_dict(
+        'prices_on_world_market_of_group_b_products'] = prices_group_b_products.to_dict(
         'index')
     result_to_front[
-        'calculation_of_amount_of_export_customs_duty'] = calculation_of_amount_of_export_customs_duty.to_dict(
+        'calculation_of_amount_of_export_customs_duty'] = calc_export_customs_duty.to_dict(
         'index')
     result_to_front['domestic_market_of_group_b_products'] = domestic_market_of_group_b_products.to_dict('index')
     result_to_front['internal_market_of_product_a'] = internal_market_of_product_a.to_dict('index')
-    result_to_front['internal_production_and_balance_of_goods_a'] = internal_production_and_balance_of_goods_a.to_dict(
+    result_to_front['internal_production_and_balance_of_goods_a'] = int_prod_balance_of_goods_a.to_dict(
         'index')
     result_to_front['internal_production_of_goods_c'] = internal_production_of_goods_c.to_dict('index')
-    result_to_front['production_and_balance_of_group_b_goods'] = production_and_balance_of_group_b_goods.to_dict('index')
+    result_to_front['production_and_balance_of_group_b_goods'] = production_and_balance_of_group_b_goods.to_dict(
+        'index')
     result_to_front['prices'] = prices.to_dict('index')
     result_to_front['production_and_consumption'] = production_and_consumption.to_dict('index')
     result_to_front['cost_effects'] = cost_effects.to_dict('index')
